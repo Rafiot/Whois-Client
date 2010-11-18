15efe6a6f533cba2ab8451c3563baed9c0ada5e5
Raphaël Vinot <raphael@vinot.info>
2010-11-18 16:43:29 +0100

    Whois fetcher fully implemented

---
+++ install/init_assignations.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Script initializing the redis database of assignations, used by the sorting processes. 
+It defines also the options of the differents servers. 
+"""
+
+import re
+import IPy
+
+# 'address' : option(s)
+whois_pre_options = {
+    #'whois.ripe.net' :  '-B ', 
+    'riswhois.ripe.net' : '-M '
+    }
+
+whois_keepalive_options = {
+    'whois.ripe.net' : '-k ', 
+    'riswhois.ripe.net' : '-k '
+    }
+    
+whois_post_options = {
+    'whois.nic.ad.jp' : ' /e '
+    }
+
+whois_port_options = {}
+
+assign_table = {}
+set_urls = set()
+url_options = {}
+
+def parse(assignations):
+    for ip,url in assignations:
+        if url not in ['UNALLOCATED', '6to4', 'teredo', '6bone', 'v6nic']:
+            if not re.findall('\.',url):
+                url = 'whois.' + url + '.net'
+        # All the urls has to be pushed in the list: 
+        # elsewhere there is no running process to put them out of redis...
+        set_urls.add(url)
+        # Buggy networks
+        if ip == '210.71.128.0/16':
+            ip = '210.71.128.0/17'
+        if ip == '210.241.0.0/15':
+            ip = '210.241.0.0/16'
+        if ip == '221.138.0.0/13':
+            ip = '221.138.0.0/15'
+        net = IPy.IP(ip)
+        assign_table[str(net)] = url
+
+def set_options():
+    for url in set_urls:
+        url_options[url] = ['', '', '', '']
+        
+        pre = whois_pre_options.get(url,  '')
+        post = whois_post_options.get(url,  '')
+        keepalive = whois_keepalive_options.get(url,  '')
+        port = whois_port_options.get(url,  43)
+        if pre:
+            url_options[url][0] = pre
+        if post:
+            url_options[url][1] = post
+        if keepalive:
+            url_options[url][2] = keepalive
+        if port:
+            url_options[url][3] = port
+
+if __name__ == "__main__":
+    import os 
+    import sys
+    import ConfigParser
+    config = ConfigParser.RawConfigParser()
+    config.read("../etc/whois_client.conf")
+    
+    regex_ipv4 = '([^#][\d./]*)'
+    regex_ipv6 = '([^#][\d\w:/]*)'
+    regex_dns  = '([^#][\d\w.]*)'
+
+    f = open('ip_del_list').read()
+    assignations = re.findall('[\n]*' + regex_ipv4 + '\t' + regex_dns + '\s*',f)
+    parse(assignations)
+
+    f = open('ip6_del_list').read()
+    assignations = re.findall('[\n]*' + regex_ipv6 + '\t' + regex_dns + '\s*',f)
+    parse(assignations)
+
+    # Self defined servers
+    # to do the RIS Requests
+    set_urls.add('riswhois.ripe.net')
+
+    set_options()
+
+    f = open(os.path.join(config.get('global','root'),config.get('global','assignations')), 'w')
+    f.write("assignation_table = %s\nwhois_servers = %s\n" % (str(assign_table), str(url_options)))
+    f.close()
+
+
---
+++ lib/fetcher/whois_fetcher.py
@@ -0,0 +1,108 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+#if __name__ == "__main__":
+import ConfigParser
+config = ConfigParser.RawConfigParser()
+config.read("../../etc/whois_client.conf")
+sleep_timer = int(config.get('global','short_timer'))
+use_syslog = config.get('global','errors_in_syslog')
+
+from socket import *
+import time
+
+from assignations import *
+
+if use_syslog:
+    import syslog
+    syslog.openlog('BGP_Ranking_Fetchers', syslog.LOG_PID, syslog.LOG_USER)
+
+class WhoisFetcher(object):
+    """Class to fetch the Whois entry of a particular IP.
+    """
+    
+    def connect(self):
+        """
+        TCP connection to one on the whois servers
+        """
+        self.s = socket(AF_INET, SOCK_STREAM)
+        self.s.connect((self.server,self.port))
+        self.connected = True
+        
+        
+    def disconnect(self):
+        """
+        Close the TCP connection 
+        """
+        self.s.close()
+        self.connected = False
+    
+    def fetch_whois(self, query, keepalive = False):
+        """
+        Fetch the whois informations. Keep the connection alive if needed. 
+        """
+        if not self.connected:
+            self.connect()
+        if keepalive:
+            self.pre_options += self.keepalive_options
+        self.s.send(self.pre_options + query + self.post_options +' \n')
+        self.text = ''
+        loop = 0
+        fs = self.s.makefile()
+        prec = ''
+        while 1:
+            temp = fs.readline()
+            if not temp or len(temp) == 0 or prec == temp == '\n':
+                break
+            self.text += temp 
+            prec = temp 
+        if len(self.text) == 0:
+            error_msg = "error (no response) with query: " + query + " on server " + self.server
+            if use_syslog:
+                syslog.syslog(syslog.LOG_ERR, error_msg)
+            else:
+                print(error_msg)
+            time.sleep(sleep_timer)
+        if not keepalive:
+            self.disconnect()
+        return self.text
+
+    def __set_values(self,  server):
+        """
+        Set the needed informations concerning the server we want to use
+        """
+        self.server = server
+        options = whois_servers[server]
+        self.pre_options = options[0]
+        self.post_options = options[1] 
+        self.keepalive_options = options[2]
+        self.port = options[3]
+
+    def __init__(self, server):
+        self.__set_values(server)
+        self.connected = False
+    
+    def __repr__(self):
+        return self.text
+
+if __name__ == "__main__":
+    f = WhoisFetcher('whois.arin.net')
+    print(f.fetch_whois('127.0.0.1', False))
+    print(f.fetch_whois('127.0.0.1', False))
+    print(f.fetch_whois('127.0.0.1', False))
+    f = WhoisFetcher('whois.ripe.net')
+    print(f.fetch_whois('127.0.0.1', True))
+    print(f.fetch_whois('127.0.0.1', True))
+    print(f.fetch_whois('127.0.0.1', False))
+    f = WhoisFetcher('whois.lacnic.net')
+    print(f.fetch_whois('200.3.14.10', False))
+    
+    f = WhoisFetcher('whois.apnic.net')
+    print(f.fetch_whois('116.66.203.208', False))
+    
+    f = WhoisFetcher('riswhois.ripe.net')
+    print(f.fetch_whois('200.3.14.10', False))
+  
+    
+    
