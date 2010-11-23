00080d6192847a49628c9deffd5d27c8d51d11a4
Raphaël Vinot <raphael.vinot@conostix.com>
2010-11-23 12:19:12 +0100

    fix the connector

--- lib/fetcher/connector.py
+++ lib/fetcher/connector.py
@@ -23,7 +23,7 @@ def get_server(query):
         if subnet in to_return:
             if query in subnet:
                 to_return = subnet
-    return to_return 
+    return assignation_table[str(to_return)]
 
 from whois_fetcher import *
 
@@ -59,20 +59,25 @@ class Connector(object):
                 to_return[server] = [query]
             else:
                 to_return[server].append(query)
+        return to_return
         
     def fetch(self):
-        responses = {}
+        self.responses = {}
         for server, queries in self.queries.iteritems():
             if len(queries) > 1 and server in support_keepalive:
                 self.keepalive = True
             try:
                 f = WhoisFetcher(server)
                 for query in queries:
-                    responses[query] = [server, f.fetch_whois(query, self.keepalive)]
+                    self.responses[query] = [server, f.fetch_whois(query, self.keepalive)]
             except IOError as text:
                 error_msg = "IOError on " + self.server + ': ' + str(text)
                 if use_syslog:
                     syslog.syslog(error_msg)
                 else:
                     print(error_msg)                
-        return responses
+        return self.responses
+
+if __name__ == "__main__":
+    c = Connector('127.0.0.1')
+    print c.fetch()
